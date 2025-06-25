181faf5cd0c839c1ec6004ee7d3398390ec09054
Fahim <ahmed.fahim37@gmail.com>
2025-06-25 16:59:52 +0600

    pricing section

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -3,6 +3,7 @@ import Decorator from "@/components/Home/Decorator";
 import Faq from "@/components/Home/Faq";
 import Hero from "@/components/Home/Hero";
 import HowItWorks from "@/components/Home/HowItWorks";
+import PricingSection from "@/components/Home/PricingSection";
 import WebsiteName from "@/components/Home/WebsiteName";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <HowItWorks />
       <Faq />
       <Decorator />
+      <PricingSection />
     </div>
   );
 }
